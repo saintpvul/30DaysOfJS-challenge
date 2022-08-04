6695d95ddf16e329bfaa1191ea63a4fb8771a4c5
saintpvul <saintpvul@gmail.com>
2022-08-04 23:34:12 +0600

    feat: complete day1 ex_1_4

---
+++ day#1/js/variable.js
@@ -0,0 +1,14 @@
+// Day 1: Exercises
+
+/*
+
+Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+*/
+
+// Solution
+
+let stringVar = "string";
+let boolVar = true;
+let undefinedVar = undefined;
+let nullVar = null;
