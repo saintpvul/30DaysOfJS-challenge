ad0bb64d5701f31a1de66335847332017710a4d7
saintpvul <saintpvul@gmail.com>
2022-08-04 23:37:11 +0600

    feat: complete day1 ex_1_5

---
+++ day#1/js/datatypes.js
@@ -0,0 +1,18 @@
+// Day 1: Exercises
+
+/*
+
+Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+*/
+
+// Solution
+
+let stringVar = "string";
+let boolVar = true;
+let undefinedVar = undefined;
+let nullVar = null;
+console.log(typeof stringVar);
+console.log(typeof boolVar);
+console.log(typeof undefinedVar);
+console.log(typeof nullVar);
