07ef62ae59dace7cbc9ab2f4059241ea9907de23
saintpvul <saintpvul@gmail.com>
2022-08-04 23:37:58 +0600

    feat: complete day1 ex_1_6

--- day#1/js/datatypes.js
+++ day#1/js/datatypes.js
@@ -2,17 +2,12 @@
 
 /*
 
-Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
-
+Declare four variables without assigning values
 */
 
 // Solution
 
-let stringVar = "string";
-let boolVar = true;
-let undefinedVar = undefined;
-let nullVar = null;
-console.log(typeof stringVar);
-console.log(typeof boolVar);
-console.log(typeof undefinedVar);
-console.log(typeof nullVar);
+let var1;
+let var2;
+let var3;
+let var4;
