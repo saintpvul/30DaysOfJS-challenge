ffd2544bea444ebe988bd8c9aa78085dd1ac1002
saintpvul <saintpvul@gmail.com>
2022-08-05 23:19:35 +0600

    feat: complete day2 ex_1_21

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,12 +2,12 @@
 
 /*
 
-Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+Use startsWith() method with the string 30 Days Of JavaScript and make the result true
 
 */
 
 
 // Solution
 
-let jsStr = ' 30 Days Of JavaScript '
-console.log(jsStr.trim())
+let jsStr = '30 Days Of JavaScript'
+console.log(jsStr.startsWith('30'))
