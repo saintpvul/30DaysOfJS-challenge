89922d12cc42ef1128a6e1bc5931c0c884361073
saintpvul <saintpvul@gmail.com>
2022-08-05 23:11:48 +0600

    feat: complete day2 ex_1_17

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -1,18 +1,14 @@
-let challenge = "30 Days Of JavaScript"
-console.log(challenge)
-console.log(challenge.length)
-console.log(challenge.toUpperCase())
-console.log(challenge.toLowerCase())
-console.log(challenge.substring(2, 21))
-console.log(challenge.slice(0, 2))
-console.log(challenge.includes('Script'))
-console.log(challenge.split(""))
-console.log(challenge.split(" "))
+// DAY 2
 
-let ITCompanies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
-console.log(ITCompanies.split(','))
-console.log(challenge.replace('JavaScript', 'Python'))
-console.log(challenge.charAt(15))
-console.log(challenge.charCodeAt('J'))
-console.log(challenge.indexOf('a'))
-console.log(challenge.lastIndexOf('a'))
+/*
+
+Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+
+
+*/
+
+
+// Solution
+
+let findBecause = 'You cannot end a sentence with because because because is a conjunction'
+console.log(findBecause.indexOf('because'))
