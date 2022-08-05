3dd7315eb653a8c92ea85ad64f6735df1309f765
saintpvul <saintpvul@gmail.com>
2022-08-05 23:13:14 +0600

    feat: complete day2 ex_1_18

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,7 +2,7 @@
 
 /*
 
-Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
 
 */
@@ -11,4 +11,4 @@ Use indexOf to find the position of the first occurrence of the word because in 
 // Solution
 
 let findBecause = 'You cannot end a sentence with because because because is a conjunction'
-console.log(findBecause.indexOf('because'))
+console.log(findBecause.lastIndexOf('because'))
