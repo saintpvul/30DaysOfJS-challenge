4d6bdb946215f776826a2a21e9e8f2fb65d77d1d
saintpvul <saintpvul@gmail.com>
2022-08-06 02:00:03 +0600

    feat: start day2 ex_2_2

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -13,3 +13,5 @@ The quote 'There is no exercise better for the heart than reaching down and lift
 // Solution
 
 console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
+
+// !start ex 2_2
