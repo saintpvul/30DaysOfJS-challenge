e32e13cced4c5f6ea94e51e3caed9cc0d887a481
saintpvul <saintpvul@gmail.com>
2022-08-05 23:25:31 +0600

    feat: complete day2 ex_2_1

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,12 +2,14 @@
 
 /*
 
-Use repeat() method to print 30 Days Of JavaScript 2 times
+Using console.log() print out the following statement:
+
+The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.
+
 
 */
 
 
 // Solution
 
-let jsDays = '30 Days Of JavaScript'
-console.log(jsDays.repeat(2))
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.')
