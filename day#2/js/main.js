a0181806daba6cc7ccf936c9d63d2771684a3aa2
saintpvul <saintpvul@gmail.com>
2022-08-06 23:12:04 +0600

    feat: complete day 2 ex_2_12

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,18 +2,14 @@
 
 /*
 
-Use console.log() and escape characters to print the following pattern.
-
-1 1 1 1 1
-2 1 2 4 8
-3 1 3 9 27
-4 1 4 16 64
-5 1 5 25 125
+Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
 */
 
 // Solution
 
-console.log(
-  "1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125"
-);
+let str =
+  "You cannot end a sentence with because because because is a conjunction";
+let strSubsOne = str.substring(0, 39);
+let strSubsTwo = str.substring(55, 71);
+console.log(strSubsOne.concat(strSubsTwo));
