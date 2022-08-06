0eba9444fd9cc87bbc159fd19c0ffc22ad266bf8
saintpvul <saintpvul@gmail.com>
2022-08-06 22:29:18 +0600

    feat: complete day 2 ex_2_4

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,13 +2,12 @@
 
 /*
 
-Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
 
 */
 
 // Solution
 
-let ten = 10;
-console.log(typeof ten);
-if (10 === 10) console.log(true);
-else console.log(parseInt(ten));
+let almostTen = 9.8;
+if (parseFloat(almostTen) === 10) console.log(true);
+else console.log(almostTen + " is " + Math.ceil(almostTen) + " now.");
