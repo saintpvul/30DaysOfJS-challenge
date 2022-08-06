0aa00ee7284b437fe3a114e169a24c4dc5572fd9
saintpvul <saintpvul@gmail.com>
2022-08-06 23:30:08 +0600

    feat: complete day 2 ex_3_4

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,15 +2,18 @@
 
 /*
 
-Clean the following text and find the most frequent word (hint, use replace and regular expressions).
-
- const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 
 */
 
 // Solution
 
-const sentence =
-  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+const income =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
 
-console.log(sentence.replace(/[`~@#$%^&*()_|+\-=;:'"<>\{\}\[\]\\\/]/gi, ""));
+let getNum = income.match(/(\d+)/gi);
+console.log(getNum);
+let a = parseInt(getNum[0]);
+let b = parseInt(getNum[1]);
+let c = parseInt(getNum[2]);
+console.log("His annuale income is " + ((a + c) * 12 + b));
