bd12752e7e42d6f1120d39fe8c33b046a44d05af
saintpvul <saintpvul@gmail.com>
2022-08-06 23:06:03 +0600

    feat: complete day 2 ex_2_11

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,11 +2,18 @@
 
 /*
 
-Access the 'JavaScript' string characters using a random number.
+Use console.log() and escape characters to print the following pattern.
+
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
 
 */
 
 // Solution
 
-let str = "JavaScript";
-console.log(str.charAt(Math.floor(Math.random() * str.length)));
+console.log(
+  "1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125"
+);
