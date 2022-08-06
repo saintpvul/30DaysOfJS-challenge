62e7020f9ca5865c117bf3d2729ef467e4c30fcf
saintpvul <saintpvul@gmail.com>
2022-08-06 23:15:30 +0600

    feat: complete day 2 ex_3_2

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,7 +2,7 @@
 
 /*
 
-'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
 
 */
 
