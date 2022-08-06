e99080580ab3996310078169467c3b0c86612513
saintpvul <saintpvul@gmail.com>
2022-08-06 22:23:54 +0600

    feat: complete day 2 ex_2_3

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -10,7 +10,7 @@ Using console.log() print out the following quote by Mother Teresa:
 
 // Solution
 
-console.log(
-  "\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\""
-);
--A;
+let ten = 10;
+console.log(typeof ten);
+if (10 === 10) console.log(true);
+else console.log(parseInt(ten));
