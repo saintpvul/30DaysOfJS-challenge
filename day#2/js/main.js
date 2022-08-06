75599ef39b376fbc57ad0f3a8fc41a29b786aa3f
saintpvul <saintpvul@gmail.com>
2022-08-06 23:24:00 +0600

    feat: complete day 2 ex_3_3

--- day#2/js/main.js
+++ day#2/js/main.js
@@ -2,12 +2,15 @@
 
 /*
 
-Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+
+ const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
 
 */
 
 // Solution
 
-let str =
-  "You cannot end a sentence with because because because is a conjunction";
-console.log(str.match(/because/gi));
+const sentence =
+  "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+
+console.log(sentence.replace(/[`~@#$%^&*()_|+\-=;:'"<>\{\}\[\]\\\/]/gi, ""));
