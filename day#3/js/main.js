204882989a89f711aa85052fcd489f2749361b12
saintpvul <saintpvul@gmail.com>
2022-08-08 02:09:39 +0600

    feat: complete day 3 ex_3_1

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -2,24 +2,20 @@
 
 /*
 
-Create a human readable time format using the Date time object
+Create a human readable time format using the Date time object. The hour and the minute should be all the time two digits(7 hours should be 07 and 5 minutes should be 05 )
+
+i. YYY-MM-DD HH:mm eg. 20120-01-02 07:05
 
 */
 
 // Solutionc
 
 let date = new Date();
-let year = date.getFullYear();
+let year = ("0" + (date.getFullYear() + 1)).slice(-3);
 let month = ("0" + (date.getMonth() + 1)).slice(-2);
 let day = ("0" + (date.getDay() + 1)).slice(-2);
 let hour = ("0" + (date.getHours() + 1)).slice(-2);
-let minute = ("0" + (date.getMinutes() + 1)).slice(-2);
+let minute = ("0" + (date.getMinutes() + 1)).slice(-3);
 
 let i = year + "-" + month + "-" + day + " " + hour + ":" + minute;
 console.log(i);
-
-let ii = day + "-" + month + "-" + year + " " + hour + ":" + minute;
-console.log(ii);
-
-let iii = day + "/" + month + "/" + year + " " + hour + ":" + minute;
-console.log(iii);
