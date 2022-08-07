84783fc29a02827199a824f6a5764d5251bb2061
saintpvul <saintpvul@gmail.com>
2022-08-07 23:04:41 +0600

    feat: complete day 3 ex_1_2

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -2,34 +2,12 @@
 
 /*
 
-Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+Check if type of '10' is equal to 10
 
 */
 
 // Solution
 
-let firstName = "Paul",
-  lastName = "Shvets",
-  country = "Kazakhstan",
-  city = "Almaty",
-  age = 27,
-  isMarried = true,
-  now = new Date(),
-  year = now.getFullYear();
-
-console.log(
-  "Name " + firstName,
-  typeof firstName,
-  ". Last name " + lastName,
-  typeof lastName,
-  ". Country " + country,
-  typeof country,
-  ". City " + city,
-  typeof city,
-  ". Age " + age,
-  typeof age,
-  ". Married " + isMarried,
-  typeof isMarried,
-  ". Year " + year,
-  typeof year
-);
+let ten = "10";
+if (typeof ten === 10) console.log("True");
+else console.log("Not true");
