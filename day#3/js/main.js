c264354e1cdfb895d80648e500ee25b738337aa1
saintpvul <saintpvul@gmail.com>
2022-08-07 23:36:53 +0600

    feat: complete day 3 ex_1_7

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -2,37 +2,26 @@
 
 /*
 
-Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+Use the Date object to do the following activities
 
 */
 
 // Solution
-
-let i = 4 > 3 && 10 < 12; // true
-let ii = 4 >= 3 && 10 > 12; // false
-let iii = 4 < 3 || 10 < 12; // true
-let iv = 4 <= 3 || 10 > 12; //false
-let v = !(4 > 3); // false
-let vi = !(4 < 3); // true
-let vii = !false; // true
-let viii = !(4 > 3 && 10 < 12); //false
-let ix = !(4 > 3 && 10 > 12); // true
-let x = !(4 === "4"); //true
-let xi = 4 === "4"; // false
-let python = "python",
-  dragon = "dragon"; // true
+let date = new Date();
+let i = date.getFullYear();
+let ii = date.getMonth();
+let iii = date.getDate();
+let iv = date.getDay();
+let v = date.getHours();
+let vi = date.getMinutes();
+let vii = date.getTime();
 
 console.log(
-  i,
-  ii,
-  iii,
-  iv,
-  v,
-  vi,
-  vii,
-  viii,
-  ix,
-  x,
-  xi,
-  python.includes("on") && dragon.includes("on")
+  "Year " + i,
+  "Month " + ii,
+  "Date " + iii,
+  "Day " + iv,
+  "Hours " + v,
+  "Minutes " + vi,
+  "Time from january 1970 " + vii
 );
