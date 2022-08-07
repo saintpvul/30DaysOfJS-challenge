736ac23b6e855daea3f9476de36245efab3bf65c
saintpvul <saintpvul@gmail.com>
2022-08-08 00:41:24 +0600

    feat: complete day 3 ex_2_9

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -2,20 +2,13 @@
 
 /*
 
-Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+Writ a script that prompt a user to enter hours and rate per hour. Calculate pay of the person?
 
 
 */
 
-// Solution
-let a = prompt("Enter radius: ");
-let PI = 3.14;
-let area = PI * +a * +a;
-let circumference = 2 * PI * +a;
+// Solutionc
 
-console.log(
-  "The area of circle is " +
-    area +
-    ". And circumference of circle is " +
-    circumference
-);
+let hours = prompt("Enter hours: ");
+let rate = prompt("Enter rate per hour: ");
+console.log("Your weekly earning is " + +hours * +rate);
