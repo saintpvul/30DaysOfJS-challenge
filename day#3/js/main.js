d16852f1ab479eb946f321aa9cb1ac59332de46f
saintpvul <saintpvul@gmail.com>
2022-08-07 23:05:42 +0600

    feat: complete day 3 ex_1_3

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -8,6 +8,7 @@ Check if type of '10' is equal to 10
 
 // Solution
 
-let ten = "10";
-if (typeof ten === 10) console.log("True");
+let almostTen = "9.8";
+if (parseInt(almostTen) === 10) console.log("True");
 else console.log("Not true");
+console.log(parseInt(almostTen));
