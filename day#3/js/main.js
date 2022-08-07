e579dfbfc121ebd6855b8a94761da23810c065a2
saintpvul <saintpvul@gmail.com>
2022-08-08 02:13:47 +0600

    feat: complete day 3 ex_2_8

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -9,13 +9,13 @@ Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and fi
 // Solutionc
 
 let x1 = 13;
-let y1 = x * 2 + 6 * x + 9;
+let y1 = x1 * 2 + 6 * x1 + 9;
 console.log(y1);
 
 let x2 = 31;
-let y2 = x * 2 + 6 * x + 9;
+let y2 = x2 * 2 + 6 * x2 + 9;
 console.log(y2);
 
 let x3 = 1;
-let y3 = x * 2 + 6 * x + 9;
+let y3 = x3 * 2 + 6 * x3 + 9;
 console.log(y3);
