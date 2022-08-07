fe6403a9aa34baf3c2290a0ca6e7278eba33f45e
saintpvul <saintpvul@gmail.com>
2022-08-08 02:17:14 +0600

    feat: complete day 3 ex_2_5(not sure)

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -2,20 +2,12 @@
 
 /*
 
-Calculate the value of y (y = x2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
+Calculate the slope, x-intercept and y-intercept of y = 2x -2
 
 */
 
 // Solutionc
 
 let x1 = 13;
-let y1 = x1 * 2 + 6 * x1 + 9;
+let y1 = 2 * x1 - 2;
 console.log(y1);
-
-let x2 = 31;
-let y2 = x2 * 2 + 6 * x2 + 9;
-console.log(y2);
-
-let x3 = 1;
-let y3 = x3 * 2 + 6 * x3 + 9;
-console.log(y3);
