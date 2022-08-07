382f0174887750e83e99ed4f1bd830d5dc64ab8b
saintpvul <saintpvul@gmail.com>
2022-08-07 22:58:46 +0600

    feat: complete day 3 ex_1_1

--- day#3/js/main.js
+++ day#3/js/main.js
@@ -1,9 +1,35 @@
-//
+// Day 3
 
 /*
 
-
+Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
 
 */
 
 // Solution
+
+let firstName = "Paul",
+  lastName = "Shvets",
+  country = "Kazakhstan",
+  city = "Almaty",
+  age = 27,
+  isMarried = true,
+  now = new Date(),
+  year = now.getFullYear();
+
+console.log(
+  "Name " + firstName,
+  typeof firstName,
+  ". Last name " + lastName,
+  typeof lastName,
+  ". Country " + country,
+  typeof country,
+  ". City " + city,
+  typeof city,
+  ". Age " + age,
+  typeof age,
+  ". Married " + isMarried,
+  typeof isMarried,
+  ". Year " + year,
+  typeof year
+);
