3708d1adddfba42128cc89d603b98e9ade214994
saintpvul <saintpvul@gmail.com>
2022-08-09 23:50:21 +0600

    update: start day 5 ex. complete day 5 ex_1_5

--- day#5/js/main.js
+++ day#5/js/main.js
@@ -2,13 +2,16 @@
 
 /*
 
-Get the first item, the middle item and the last item of the array
+Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
 
 */
 
 // Solution
 
-let arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(arr.shift());
-console.log(arr.splice(2, 1));
-console.log(arr.pop());
+let mixedDataTypes = [1, "name", true, null, 5, "6", 5 > 4];
+console.log(mixedDataTypes);
+console.log(mixedDataTypes.length);
+console.log(typeof mixedDataTypes[0]);
+console.log(typeof mixedDataTypes[5]);
+console.log(typeof mixedDataTypes[7]);
+console.log(typeof mixedDataTypes[3]);
