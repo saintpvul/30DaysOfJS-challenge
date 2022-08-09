e07c1b137c0bed7769e9f3669e57b01811323d9c
saintpvul <saintpvul@gmail.com>
2022-08-09 23:51:41 +0600

    update: start day 5 ex. complete day 5 ex_1_6

--- day#5/js/main.js
+++ day#5/js/main.js
@@ -2,16 +2,18 @@
 
 /*
 
-Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
 
 */
 
 // Solution
 
-let mixedDataTypes = [1, "name", true, null, 5, "6", 5 > 4];
-console.log(mixedDataTypes);
-console.log(mixedDataTypes.length);
-console.log(typeof mixedDataTypes[0]);
-console.log(typeof mixedDataTypes[5]);
-console.log(typeof mixedDataTypes[7]);
-console.log(typeof mixedDataTypes[3]);
+let itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
