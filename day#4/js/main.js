86d8b9fc26808ac7a59935b817efcea43650a42f
saintpvul <saintpvul@gmail.com>
2022-08-09 22:16:20 +0600

    feat: complete day4 ex_1_2

--- day#4/js/main.js
+++ day#4/js/main.js
@@ -2,20 +2,18 @@
 
 /*
 
-Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
 
 Enter your age: 30
-You are old enough to drive.
-
-Enter your age:15
-You are left with 3 years to drive.
+You are 5 years older than me.
 
 */
 
 // Solution
 
-let age = prompt("Enter your age: ");
-let ok = 18;
-if (+age >= 18)
-  console.log("You are " + +age + ". You are old enough to drive.");
-else console.log("You are left with " + (ok - +age) + " to drive.");
+let yourAge = prompt("Enter your age: ");
+let myAge = prompt("My age is: ");
+if (+yourAge > myAge)
+  console.log("You are " + (+yourAge - +myAge) + " years older than me.");
+else if (+yourAge == +myAge) console.log("We are peers.");
+else console.log("I am " + (+myAge - +yourAge) + " years older than you.");
