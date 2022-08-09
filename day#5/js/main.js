461f1548e61ef78fb41181707b414471768fa497
saintpvul <saintpvul@gmail.com>
2022-08-10 00:06:51 +0600

    complete day 5 ex_1_12

--- day#5/js/main.js
+++ day#5/js/main.js
@@ -2,7 +2,7 @@
 
 /*
 
-Change each company name to uppercase one by one and print them out
+Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
 
 */
 
@@ -17,10 +17,4 @@ let itCompanies = [
   "Oracle",
   "Amazon",
 ];
-console.log(itCompanies[0].toUpperCase());
-console.log(itCompanies[1].toUpperCase());
-console.log(itCompanies[2].toUpperCase());
-console.log(itCompanies[3].toUpperCase());
-console.log(itCompanies[4].toUpperCase());
-console.log(itCompanies[5].toUpperCase());
-console.log(itCompanies[6].toUpperCase());
+console.log(itCompanies.join(", ") + " are big IT companies.");
