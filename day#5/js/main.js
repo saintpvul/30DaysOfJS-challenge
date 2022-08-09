3241a6dd4c571ddd575e54f7578177fe34100f8c
saintpvul <saintpvul@gmail.com>
2022-08-09 23:47:56 +0600

    update: start day 5 ex. complete day 5 ex_1_4

--- day#5/js/main.js
+++ day#5/js/main.js
@@ -2,11 +2,13 @@
 
 /*
 
-Find the length of your array
+Get the first item, the middle item and the last item of the array
 
 */
 
 // Solution
 
-let arr = [1, 2, 3, 4, 5, 6];
-console.log(arr.length);
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr.shift());
+console.log(arr.splice(2, 1));
+console.log(arr.pop());
