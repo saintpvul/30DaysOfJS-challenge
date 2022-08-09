fb288abf00d512c167a4409cf260e9873ef136f3
saintpvul <saintpvul@gmail.com>
2022-08-09 22:33:19 +0600

    feat: complete day4 ex_1_3.2/forgot to solve second tile

--- day#4/js/main.js
+++ day#4/js/main.js
@@ -2,28 +2,16 @@
 
 /*
 
-Write a code which can give grades to students according to theirs scores:
-80-100, A
-70-89, B
-60-69, C
-50-59, D
-0-49, F
+If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
 
+ternary operator. 
+
+!forgot to do this
 
 */
 
 // Solution
 
-function grade(score) {
-  if (score >= 0 && score <= 49) console.log("F");
-  else if (score >= 50 && score <= 59) console.log("D");
-  else if (score >= 60 && score <= 69) console.log("C");
-  else if (score >= 70 && score <= 89) console.log("B");
-  else if (score >= 90 && score <= 100) console.log("A");
-}
-
-grade(13);
-grade(67);
-grade(78);
-grade(96);
-grade(53);
+let a = 4;
+let b = 3;
+a > b ? console.log("a is greater than b") : console.log("a is less than b");
