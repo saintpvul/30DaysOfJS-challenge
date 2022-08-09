83551e04733adc059a0f97de08d9d13c4b17a15b
saintpvul <saintpvul@gmail.com>
2022-08-09 22:44:42 +0600

    feat: complete day 4 ex 2_2

--- day#4/js/main.js
+++ day#4/js/main.js
@@ -2,16 +2,32 @@
 
 /*
 
-If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in to ways
-
-ternary operator. 
-
-!forgot to do this
+Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+September, October or November, the season is Autumn.
+December, January or February, the season is Winter.
+March, April or May, the season is Spring
+June, July or August, the season is Summer
 
 */
 
 // Solution
-
-let a = 4;
-let b = 3;
-a > b ? console.log("a is greater than b") : console.log("a is less than b");
+let getMonth = prompt("Enter current month: ");
+let removeSpaces = getMonth.trim();
+let findMonth = removeSpaces.toLowerCase();
+if (
+  findMonth === "september" ||
+  findMonth === "october" ||
+  findMonth === "november"
+)
+  console.log("This is Autumn");
+else if (
+  findMonth === "december" ||
+  findMonth === "january" ||
+  findMonth === "february"
+)
+  console.log("This is Winter");
+else if (findMonth === "march" || findMonth === "april" || findMonth === "may")
+  console.log("This is Spring");
+else if (findMonth === "june" || findMonth === "july" || findMonth === "august")
+  console.log("This is Summer");
+else console.log("Is this a month? Try again");
