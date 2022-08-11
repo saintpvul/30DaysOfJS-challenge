355b4b838c868bf45f1b5589cd631134b3d70985
saintpvul <saintpvul@gmail.com>
2022-08-11 21:31:11 +0600

    complete day 5 ex_3_2

--- day#5/js/main.js
+++ day#5/js/main.js
@@ -9,13 +9,10 @@ Concatenate the following two variables and store it in a fullStack variable.
 // Solution
 
 import countries from "./countries.js";
-// console.log(countries);
+console.log(countries);
 
 import webTechs from "./web_techs.js";
-// console.log(webTechs);
+console.log(webTechs);
 
-const frontEnd = ["HTML", "CSS", "JS", "React", "Redux"];
-const backEnd = ["Node", "Express", "MongoDB"];
-
-let fullStack = frontEnd.concat(backEnd);
-console.log(fullStack);
+const midNdx = countries[Math.round((countries.length - 1) / 2)];
+console.log(midNdx);
