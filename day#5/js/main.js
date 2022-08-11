a6fc75982f478fc2075ee47ed1dacfd227f6bb5e
saintpvul <saintpvul@gmail.com>
2022-08-11 21:31:27 +0600

    complete day 5 ex_3_3

--- day#5/js/main.js
+++ day#5/js/main.js
@@ -16,3 +16,15 @@ console.log(webTechs);
 
 const midNdx = countries[Math.round((countries.length - 1) / 2)];
 console.log(midNdx);
+
+let odd = [];
+let even = [];
+for (let i = 0; i < countries.length; i++) {
+  if ((i + 2) % 2 == 0) {
+    odd.push(countries[i]);
+  } else {
+    even.push(countries[i]);
+  }
+}
+console.log(odd);
+console.log(even);
