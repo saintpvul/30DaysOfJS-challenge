9fabd1beb1de81c2f63283194d38fc1988b1eb49
saintpvul <saintpvul@gmail.com>
2022-08-12 21:25:39 +0600

    feat: complete day 6 ex_1_11

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -2,17 +2,24 @@
 
 /*
 
-Use for loop to iterate from 0 to 100 and print the sum of all numbers.
+Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
 
 
 */
 
 // Solution;
-//JavaScript program to print prime numbers from 1 to 100 using for loop
 
-let sum = 0;
+let sumOdd = 0;
+let sumEven = 0;
 for (let i = 0; i <= 100; i++) {
-  sum = i + sum;
+  if (i % 2 == 0) {
+    sumEven = i + sumEven;
+  } else sumOdd = i + sumOdd;
 }
 
-console.log("The sum of all numbers from 0 to 100 is " + sum);
+console.log(
+  "The sum of all evens from 0 to 100 is " +
+    sumEven +
+    ".  And the sum of all odds from 0 to 100 is " +
+    sumOdd
+);
