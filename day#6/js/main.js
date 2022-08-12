529dc90f2de9fd5e005bd799a93cf2ea4772d344
saintpvul <saintpvul@gmail.com>
2022-08-12 21:22:34 +0600

    feat: complete day 6 ex_1_10

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -2,7 +2,7 @@
 
 /*
 
-Use for loop to iterate from 0 to 100 and print only prime numbers
+Use for loop to iterate from 0 to 100 and print the sum of all numbers.
 
 
 */
@@ -10,19 +10,9 @@ Use for loop to iterate from 0 to 100 and print only prime numbers
 // Solution;
 //JavaScript program to print prime numbers from 1 to 100 using for loop
 
-let isPrime = true;
-
-console.log("Prime numbers from 1 to 100 are: ");
-
-for (let i = 2; i <= 100; i++) {
-  for (let j = 2; j < i - 1; j++) {
-    if (i % j == 0) {
-      isPrime = false;
-      break;
-    }
-  }
-  if (isPrime) {
-    console.log(i);
-  }
-  isPrime = true;
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum = i + sum;
 }
+
+console.log("The sum of all numbers from 0 to 100 is " + sum);
