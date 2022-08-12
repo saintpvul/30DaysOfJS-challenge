6ec270248c4e1edc34465b97d2ffbc2d2573b77a
saintpvul <saintpvul@gmail.com>
2022-08-12 21:10:46 +0600

    feat: complete day 6 ex_1_6

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -4,23 +4,25 @@
 
 Use loop to print the following pattern:
 
-0 x 0 = 0
-1 x 1 = 1
-2 x 2 = 4
-3 x 3 = 9
-4 x 4 = 16
-5 x 5 = 25
-6 x 6 = 36
-7 x 7 = 49
-8 x 8 = 64
-9 x 9 = 81
-10 x 10 = 100
+ i    i^2   i^3
+ 0    0     0
+ 1    1     1
+ 2    4     8
+ 3    9     27
+ 4    16    64
+ 5    25    125
+ 6    36    216
+ 7    49    343
+ 8    64    512
+ 9    81    729
+ 10   100   1000
 
 
 */
 
 // Solution;
 
-for (let i = 1; i < 11; i++) {
-  console.log(`${i} x ${i} = ${i * i}`);
+console.log(`i i^2 i^3`);
+for (let i = 0; i < 11; i++) {
+  console.log(`${i}  ${i ** 2} ${i ** 3}`);
 }
