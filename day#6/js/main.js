777bb4e80bd27cae765a1a1eaa7c98856018c067
saintpvul <saintpvul@gmail.com>
2022-08-12 21:28:46 +0600

    feat: complete day 6 ex_1_12

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -2,13 +2,14 @@
 
 /*
 
-Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array
 
+ [2550, 2500]
 
 */
 
 // Solution;
-
+let arr = [];
 let sumOdd = 0;
 let sumEven = 0;
 for (let i = 0; i <= 100; i++) {
@@ -16,10 +17,6 @@ for (let i = 0; i <= 100; i++) {
     sumEven = i + sumEven;
   } else sumOdd = i + sumOdd;
 }
-
-console.log(
-  "The sum of all evens from 0 to 100 is " +
-    sumEven +
-    ".  And the sum of all odds from 0 to 100 is " +
-    sumOdd
-);
+arr.push(sumEven);
+arr.push(sumOdd);
+console.log(arr);
