8b7ac41d394aed57ee3f375de5165e652996270c
saintpvul <saintpvul@gmail.com>
2022-08-12 21:35:10 +0600

    feat: complete day 6 ex_1_13

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -2,21 +2,10 @@
 
 /*
 
-Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds. Print sum of evens and sum of odds as array
-
- [2550, 2500]
+Develop a small script which generate array of 5 random numbers
 
 */
 
 // Solution;
-let arr = [];
-let sumOdd = 0;
-let sumEven = 0;
-for (let i = 0; i <= 100; i++) {
-  if (i % 2 == 0) {
-    sumEven = i + sumEven;
-  } else sumOdd = i + sumOdd;
-}
-arr.push(sumEven);
-arr.push(sumOdd);
-console.log(arr);
+
+console.log(Array.from({ length: 5 }, () => Math.floor(Math.random() * 5)));
