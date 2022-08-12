b185e3d33ba668203fda02b46a0baa433066560b
saintpvul <saintpvul@gmail.com>
2022-08-12 21:06:43 +0600

    feat: complete day 6 ex_1_5

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -17,10 +17,6 @@ Write a loop that makes the following pattern using console.log():
 
 // Solution;
 
-for (let i = 1; i < 8; i = i + 1) {
-  let str = "";
-  for (let j = 0; j < i; j = j + 1) {
-    str += "#";
-  }
-  console.log(str);
+for (let i = 1; i < 11; i++) {
+  console.log(`${i} x ${i} = ${i * i}`);
 }
