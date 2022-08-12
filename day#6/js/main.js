8263458c2c2bd25150e610960c2ff3e268e17322
saintpvul <saintpvul@gmail.com>
2022-08-12 21:15:47 +0600

    feat: complete day 6 ex_1_8

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -2,20 +2,7 @@
 
 /*
 
-Use loop to print the following pattern:
-
- i    i^2   i^3
- 0    0     0
- 1    1     1
- 2    4     8
- 3    9     27
- 4    16    64
- 5    25    125
- 6    36    216
- 7    49    343
- 8    64    512
- 9    81    729
- 10   100   1000
+Use for loop to iterate from 0 to 100 and print only odd numbers
 
 
 */
