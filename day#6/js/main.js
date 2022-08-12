84ee780d8b66676a73f3e62ebd9d91267d947b84
saintpvul <saintpvul@gmail.com>
2022-08-12 21:20:19 +0600

    feat: complete day 6 ex_1_9

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -2,15 +2,27 @@
 
 /*
 
-Use for loop to iterate from 0 to 100 and print only odd numbers
+Use for loop to iterate from 0 to 100 and print only prime numbers
 
 
 */
 
 // Solution;
+//JavaScript program to print prime numbers from 1 to 100 using for loop
 
-for (let i = 0; i < 101; i++) {
-  if (i % 2 !== 0) {
+let isPrime = true;
+
+console.log("Prime numbers from 1 to 100 are: ");
+
+for (let i = 2; i <= 100; i++) {
+  for (let j = 2; j < i - 1; j++) {
+    if (i % j == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
     console.log(i);
   }
+  isPrime = true;
 }
