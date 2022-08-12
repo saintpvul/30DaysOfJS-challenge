7427df3442ad82c8f1e282dcebc2ed087204725a
saintpvul <saintpvul@gmail.com>
2022-08-12 21:36:40 +0600

    feat: complete day 6 ex_1_14

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -8,4 +8,9 @@ Develop a small script which generate array of 5 random numbers
 
 // Solution;
 
-console.log(Array.from({ length: 5 }, () => Math.floor(Math.random() * 5)));
+var arr = [];
+while (arr.length < 5) {
+  var r = Math.floor(Math.random() * 100) + 1;
+  if (arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);
