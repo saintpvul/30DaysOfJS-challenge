2b59033262b35f0f09ff2bea83bad33c1f28da39
saintpvul <saintpvul@gmail.com>
2022-08-12 20:50:36 +0600

    feat: complete day 6 ex_1_3

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -2,25 +2,16 @@
 
 /*
 
-Iterate 10 to 0 using for loop, do the same using while and do while loop
+Iterate 0 to n using for loop
 
 
 */
 
 // Solution
 
-for (let i = 10; i >= 0; i--) {
-  console.log("For loop I", i);
+function toN(n) {
+  for (let i = 0; i <= n; i++) {
+    console.log(i);
+  }
 }
-
-let i1 = 10;
-while (i1 >= 0) {
-  console.log("While I ", i1);
-  i1--;
-}
-
-let i2 = 10;
-do {
-  console.log("Do ... while I", i2);
-  i2--;
-} while (i2 >= 0);
+toN(12);
