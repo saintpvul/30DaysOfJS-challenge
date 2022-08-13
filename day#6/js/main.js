5c328057104952371537431f77fdf72f02f619a4
saintpvul <saintpvul@gmail.com>
2022-08-14 00:31:22 +0600

    feat: complete day 6 ex_2_9

--- day#6/js/main.js
+++ day#6/js/main.js
@@ -1,25 +1,11 @@
 // DAY 6
 
 /*
-[
-  ['Albania', 'ALB', 7],
-  ['Bolivia', 'BOL', 7],
-  ['Canada', 'CAN', 6],
-  ['Denmark', 'DEN', 7],
-  ['Ethiopia', 'ETH', 8],
-  ['Finland', 'FIN', 7],
-  ['Germany', 'GER', 7],
-  ['Hungary', 'HUN', 7],
-  ['Ireland', 'IRE', 7],
-  ['Iceland', 'ICE', 7],
-  ['Japan', 'JAP', 5],
-  ['Kenya', 'KEN', 5]
-]
 
 
-In above countries array, check if there is a country or countries end with a substring 'ia'. If there are countries end with, print it as array. If there is no country containing the word 'ai', print 'These are countries ends without ia'.
+Using the above countries array, find the country containing the biggest number of characters.
 
-['Albania', 'Bolivia','Ethiopia']
+Ethiopia
 
 
 */
@@ -41,30 +27,14 @@ const countries = [
   "Kenya",
 ];
 
-let newArr = [];
-for (let i = 0; i < countries.length; i++) {
-  let toUp = countries[i].toUpperCase();
+var length = 0;
+var longest;
 
-  arrIn.push(countries[i]),
-    arrIn.push(toUp.slice(0, 3)),
-    arrIn.push(countries[i].length);
-
-  for (let j = 0; j < arrIn.length; j++) {
-    arrOut.push(arrIn.splice(0, 3));
+for (var i = 0; i < countries.length; i++) {
+  if (countries[i].length > length) {
+    var length = countries[i].length;
+    longest = countries[i];
   }
 }
 
-console.log(arrOut);
-
-function find(keyword) {
-  {
-    if (keyword == "land") {
-      console.log([countries.filter((country) => country.endsWith("land"))]);
-    } else if (keyword == "ia") {
-      console.log([countries.filter((country) => country.endsWith("ia"))]);
-    }
-  }
-}
-
-find("land");
-find("ia");
+console.log(longest);
