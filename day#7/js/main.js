bad974f4ffd45e9f8cd0ad5f9016cfb5233b2fb9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-16 02:16:08 +0600

    feat: complete day7 ex 1_14

--- day#7/js/main.js
+++ day#7/js/main.js
@@ -2,28 +2,41 @@
 
 /*
 
-Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
-
-	The same groups apply to both men and women.
-	Underweight: BMI is less than 18.5
-	Normal weight: BMI is 18.5 to 24.9
-	Overweight: BMI is 25 to 29.9
-	Obese: BMI is 30 or more
+Write a function called checkSeason, it takes a month parameter and returns the season:Autumn, Winter, Spring or Summer.
 
 */
 
 //Solution
 
-function BMI(kg, cm) {
-  let bmi = kg / ((cm * cm) / 10000);
-  if (bmi < 18.5) console.log(`Your BMI is ${bmi}, you has Underweight`);
-  else if (bmi >= 18.5 && bmi < 24.9)
-    console.log(`Your BMI is ${bmi}, you has Normal weight`);
-  else if (bmi >= 25 && bmi < 29.9)
-    console.log(`Your BMI is ${bmi}, you has Overweight`);
-  else console.log(`Your BMI is ${bmi}, you has Obese`);
+function checkSeason(month) {
+  let monthToLow = month.toLowerCase();
+  if (
+    monthToLow == "december" ||
+    monthToLow == "january" ||
+    monthToLow == "february"
+  )
+    console.log("The season is Winter");
+  else if (
+    monthToLow == "march" ||
+    monthToLow == "april" ||
+    monthToLow == "may"
+  )
+    console.log("The season is Spring");
+  else if (
+    monthToLow == "june" ||
+    monthToLow == "july" ||
+    monthToLow == "august"
+  )
+    console.log("The season is Summer");
+  else if (
+    monthToLow == "september" ||
+    monthToLow == "october" ||
+    monthToLow == "november"
+  )
+    console.log("The season is Autumn");
+  else console.log(`${month} is not a Month`);
 }
 
-BMI(80, 190);
-
-BMI(55, 165);
+checkSeason("April");
+checkSeason("September");
+checkSeason("Mai");
