542f2b50e3c194841c89c3a18b8dfda419960f65
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-16 02:19:55 +0600

    feat: complete day7 ex 1_15

--- day#7/js/main.js
+++ day#7/js/main.js
@@ -2,41 +2,26 @@
 
 /*
 
-Write a function called checkSeason, it takes a month parameter and returns the season:Autumn, Winter, Spring or Summer.
+Math.max returns its largest argument. Write a function findMax that takes three arguments and returns their maximum with out using Math.max method.
+
+	console.log(findMax(0, 10, 5))
+		10
+
+	console.log(findMax(0, -10, -2))
+		0
 
 */
 
 //Solution
 
-function checkSeason(month) {
-  let monthToLow = month.toLowerCase();
-  if (
-    monthToLow == "december" ||
-    monthToLow == "january" ||
-    monthToLow == "february"
-  )
-    console.log("The season is Winter");
-  else if (
-    monthToLow == "march" ||
-    monthToLow == "april" ||
-    monthToLow == "may"
-  )
-    console.log("The season is Spring");
-  else if (
-    monthToLow == "june" ||
-    monthToLow == "july" ||
-    monthToLow == "august"
-  )
-    console.log("The season is Summer");
-  else if (
-    monthToLow == "september" ||
-    monthToLow == "october" ||
-    monthToLow == "november"
-  )
-    console.log("The season is Autumn");
-  else console.log(`${month} is not a Month`);
+function findMax(...args) {
+  let max = Math.max(...args);
+
+  return max;
 }
 
-checkSeason("April");
-checkSeason("September");
-checkSeason("Mai");
+console.log(findMax(13, 141, 84, 123, 41));
+
+console.log(findMax(3, 2, 6));
+
+console.log(findMax(65, 55, 60));
