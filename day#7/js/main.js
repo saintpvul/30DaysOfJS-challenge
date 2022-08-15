00aaf608f4e036f2fc8c414b1178385b17bc8df2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-16 02:10:36 +0600

    feat: complete day7 ex 1_13

--- day#7/js/main.js
+++ day#7/js/main.js
@@ -2,18 +2,28 @@
 
 /*
 
-Temperature in oC can be converted to oF using this formula: oF = (oC x 9/5) + 32. Write a function which convert oC to oF convertCelsiusToFahrenheit.
+Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+
+	The same groups apply to both men and women.
+	Underweight: BMI is less than 18.5
+	Normal weight: BMI is 18.5 to 24.9
+	Overweight: BMI is 25 to 29.9
+	Obese: BMI is 30 or more
 
 */
 
 //Solution
 
-function convertCelsiusToFahrenheit(oC) {
-  let oF = (oC * 9) / 5 + 32;
-  return `o Celcium ${oC} in o Fahrenheit is ${oF}`;
+function BMI(kg, cm) {
+  let bmi = kg / ((cm * cm) / 10000);
+  if (bmi < 18.5) console.log(`Your BMI is ${bmi}, you has Underweight`);
+  else if (bmi >= 18.5 && bmi < 24.9)
+    console.log(`Your BMI is ${bmi}, you has Normal weight`);
+  else if (bmi >= 25 && bmi < 29.9)
+    console.log(`Your BMI is ${bmi}, you has Overweight`);
+  else console.log(`Your BMI is ${bmi}, you has Obese`);
 }
-console.log(convertCelsiusToFahrenheit(37));
 
-console.log(convertCelsiusToFahrenheit(0));
+BMI(80, 190);
 
-console.log(convertCelsiusToFahrenheit(18));
+BMI(55, 165);
