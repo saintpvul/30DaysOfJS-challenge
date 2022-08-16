338f4c01b518d407f445fd539f95bb647250ac1c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-17 03:30:14 +0600

    feat: complete day7 ex 2_5

--- day#7/js/main.js
+++ day#7/js/main.js
@@ -2,20 +2,19 @@
 
 /*
 
-Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
 
 */
 
 //Solution
 
-function printArray(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-  }
+function showDateTime() {
+  let date = new Date();
+  let getDay = ("0" + date.getDay()).slice(-2);
+  let getMonth = ("0" + date.getMonth()).slice(-3);
+  let getYear = date.getFullYear();
+  let getHour = ("0" + date.getHours()).slice(-2);
+  let getMin = ("0" + date.getMinutes()).slice(-2);
+  console.log(`${getDay}/${getMonth}/${getYear} ${getHour}:${getMin}`);
 }
-
-printArray([1, 2, 3]);
-
-printArray(["Yetayah", "Miguel", "Paul"]);
-
-printArray([true, 1, null]);
+showDateTime();
