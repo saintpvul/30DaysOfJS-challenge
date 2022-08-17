1b9818bdb031fd7d5b4c9606f30aa9b6744ef506
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-17 22:01:30 +0600

    feat: complete day7 ex 3_1//cant print diff ids multiple times using promt

--- day#7/js/main.js
+++ day#7/js/main.js
@@ -2,10 +2,23 @@
 
 /*
 
-Declare a function name userIdGenerator. When this function is called it generates seven character id. The function return the id.
+Modify the userIdGenerator function. Declare a function name userIdGeneratedByUser. It doesn’t take any parameter but it takes two inputs using prompt(). One of the input is the number of characters and the second input is the number of ids which are supposed to be generated.
 
-console.log(userIdGenerator());
-41XTDbE
+userIdGeneratedByUser()
+'kcsy2
+SMFYb
+bWmeq
+ZXOYh
+2Rgxf
+'
+userIdGeneratedByUser()
+'1GCSgPLMaBAVQZ26
+YD7eFwNQKNs7qXaT
+ycArC5yrRupyG00S
+UbGxOFI7UXSWAyKN
+dIV0SSUTgAdKwStr
+'
+
 
 */
 
@@ -15,7 +28,10 @@ function userIdGenerator() {
   let text = "";
   let char = "01234567890abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  for (let i = 0; i < 7; i++)
+  let length = prompt("Enter number of characters you needed: ");
+  let idNum = prompt("Enter number of IDs you needed: ");
+
+  for (let i = 0; i < +length; i++)
     text += char.charAt(Math.floor(Math.random() * char.length));
 
   return text;
