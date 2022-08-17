e2029cdbc0e6d92b1c2b002596e956299751a08d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-17 22:24:17 +0600

    feat: complete day7 ex 3_2

--- day#7/js/main.js
+++ day#7/js/main.js
@@ -2,39 +2,20 @@
 
 /*
 
-Modify the userIdGenerator function. Declare a function name userIdGeneratedByUser. It doesn’t take any parameter but it takes two inputs using prompt(). One of the input is the number of characters and the second input is the number of ids which are supposed to be generated.
+Write a function name rgbColorGenerator and it generates rgb colors.
 
-userIdGeneratedByUser()
-'kcsy2
-SMFYb
-bWmeq
-ZXOYh
-2Rgxf
-'
-userIdGeneratedByUser()
-'1GCSgPLMaBAVQZ26
-YD7eFwNQKNs7qXaT
-ycArC5yrRupyG00S
-UbGxOFI7UXSWAyKN
-dIV0SSUTgAdKwStr
-'
-
+rgbColorGenerator()
+rgb(125,244,255)
 
 */
 
 //Solution
 
-function userIdGenerator() {
-  let text = "";
-  let char = "01234567890abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  let length = prompt("Enter number of characters you needed: ");
-  let idNum = prompt("Enter number of IDs you needed: ");
-
-  for (let i = 0; i < +length; i++)
-    text += char.charAt(Math.floor(Math.random() * char.length));
-
-  return text;
+function randNum(num) {
+  return Math.floor(Math.random() * num);
+}
+function rgbColorGenerator() {
+  return `rgb(${randNum(255)},${randNum(255)},${randNum(255)})`;
 }
 
-console.log(userIdGenerator());
+console.log(rgbColorGenerator());
