8128969c46e7a119b27b848bd7405e1d4a718deb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-19 00:01:44 +0600

    feat: start ex 1_3

--- day#8/js/main.js
+++ day#8/js/main.js
@@ -2,11 +2,19 @@
 
 /*
 
-Print the the dog object on the console
+Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+!START
 
 
 */
 
 // Solution
-const dog = {};
-console.log(dog);
+const dog = {
+  name: "Otto",
+  legs: 4,
+  color: "black",
+  age: 5,
+  bark: "woof woof",
+};
+const bark = Object.keys(dog.bark);
+console.log(bark);
