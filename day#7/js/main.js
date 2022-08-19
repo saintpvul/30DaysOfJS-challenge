a0a665176d3fc787088b0bb5343a16faa39146d5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-08-19 22:49:35 +0600

    feat: complete day 7 ex 3_14

--- day#7/js/main.js
+++ day#7/js/main.js
@@ -2,26 +2,24 @@
 
 /*
 
-Write a function called sumOfArrayItems, it takes an array parameter and return the sum of all the items. Check if all the array items are number types. If not give return reasonable feedback.
-
-
+Write a function called modifyArray takes array as parameter and modifies the fifth item of the array and return the array. If the array length is less than five it return 'item not found'.
 
 */
 
 //Solution
 
-function sumOfArrayItems(param) {
-  let total = 0;
-  if (param.every((elem) => typeof elem == "number")) {
-    total = param.reduce((a, b) => a + b, 0);
-    return total;
-  } else if (
-    !param.every((elem) => elem == undefined && typeof elem == "number")
-  ) {
-    return "Not all items are numbers";
-  }
+function modifyArray(arr) {
+  if (arr.length > 4) {
+    arr[4] = arr[4].toUpperCase();
+    return arr;
+  } else return "item not found";
 }
+console.log(
+  modifyArray(["Avocado", "Tomato", "Potato", "Mango", "Lemon", "Carrot"])
+);
 
-console.log(sumOfArrayItems([1, 2, 3]));
+console.log(
+  modifyArray(["Google", "Facebook", "Apple", "Amazon", "Microsoft", "IBM"])
+);
 
-console.log(sumOfArrayItems(["Paul", 1, 2]));
+console.log(modifyArray(["Google", "Facebook", "Apple", "Amazon"]));
