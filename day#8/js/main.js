d627057cd97b09fa5d46d1691994fe78d1cdbd6f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-12-21 00:50:08 +0600

    feat: complete day 8. ex lvl 3. ex2b

--- day#8/js/main.js
+++ day#8/js/main.js
@@ -71,6 +71,13 @@ const products = [
   },
 ];
 
+/*
+
+Imagine you are getting the above users collection from a MongoDB database. a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
+b. Create a function called signIn which allows user to sign in to the application
+
+*/
+
 function signUp() {
   let login = "",
     name = "",
